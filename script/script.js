b6acd989a6cedf0085ebbd07c9d39f342a391ea4
Shizo122016 <79899088+Shizo122016@users.noreply.github.com>
2021-04-21 15:29:39 +0200

    dasdsa

--- script/script.js
+++ script/script.js
@@ -26,9 +26,9 @@ function showSlides(n){
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i ++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" aktywny", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    dots[slideIndex-1].className += " aktywny";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
